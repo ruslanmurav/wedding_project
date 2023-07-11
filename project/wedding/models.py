500c91e2b74383311fda34ba506a896a1160f7d5
ruslanmurav <ruslanmuraveiko@gmail.com>
2023-07-11 20:11:46 +0300

    *add Comment model

--- project/wedding/models.py
+++ project/wedding/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
-
 class Wedding(models.Model):
     bride_name = models.CharField(max_length=255)
     groom_name = models.CharField(max_length=255)
@@ -12,3 +9,16 @@ class Wedding(models.Model):
 
     def __str__(self):
         return f"{self.bride_name} & {self.groom_name}'s Wedding"
+
+
+class Comment(models.Model):
+    comment_id = models.AutoField(primary_key=True)  # Unique identifier for the comment
+    wedding = models.ForeignKey(Wedding, on_delete=models.CASCADE)
+    commenter_name = models.CharField(max_length=255)
+    commenter_avatar = models.URLField()
+    comment_text = models.TextField()
+    comment_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment {self.comment_id} by {self.commenter_name} on {self.wedding.bride_name} & {self.wedding.groom_name}'s Wedding"
+
