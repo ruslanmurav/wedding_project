5a050dd9c24f6aa3f117ee4c2245fcf4df4131ed
ruslanmurav <ruslanmuraveiko@gmail.com>
2023-07-11 20:07:10 +0300

    *make model Wedding

--- project/wedding/models.py
+++ project/wedding/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Wedding(models.Model):
+    bride_name = models.CharField(max_length=255)
+    groom_name = models.CharField(max_length=255)
+    wedding_date = models.DateTimeField()
+    wedding_location = models.CharField(max_length=255)
+    wedding_description = models.TextField()
+
+    def __str__(self):
+        return f"{self.bride_name} & {self.groom_name}'s Wedding"
