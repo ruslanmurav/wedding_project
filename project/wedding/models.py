8a4a3f3a65e9d8494c010070e7135873ca379abf
ruslanmurav <ruslanmuraveiko@gmail.com>
2023-07-12 14:49:33 +0300

    *create templates dir

--- project/wedding/models.py
+++ project/wedding/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Wedding(models.Model):
     bride_name = models.CharField(max_length=255)
     groom_name = models.CharField(max_length=255)
@@ -12,6 +13,7 @@ class Wedding(models.Model):
 
 
 class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
     wedding = models.ForeignKey(Wedding, on_delete=models.CASCADE)
     commenter_name = models.CharField(max_length=255)
     commenter_avatar = models.URLField()
@@ -21,11 +23,11 @@ class Comment(models.Model):
     def __str__(self):
         return f"Comment by {self.commenter_name} on {self.wedding.bride_name} & {self.wedding.groom_name}'s Wedding"
 
-    class Photo(models.Model):
-        wedding = models.ForeignKey(Wedding, on_delete=models.CASCADE)
-        photo_url = models.URLField()
-        photo_description = models.CharField(max_length=255)
 
-        def __str__(self):
-            return f"Photofor {self.wedding.bride_name} & {self.wedding.groom_name}'s Wedding"
+class Photo(models.Model):
+    wedding = models.ForeignKey(Wedding, on_delete=models.CASCADE)
+    photo_url = models.URLField()
+    photo_description = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"Photo for {self.wedding.bride_name} & {self.wedding.groom_name}'s Wedding"
