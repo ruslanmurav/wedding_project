27c31936e8fc72cbf0e94ff85702fc4354af4bc2
ruslanmurav <ruslanmuraveiko@gmail.com>
2023-07-13 22:03:54 +0300

    add direct

--- project/project/settings.py
+++ project/project/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = {
+STATICFILES_DIRS = [
     BASE_DIR / 'static',
-}
+]
 
 
 MEDIA_URL = '/media/'
