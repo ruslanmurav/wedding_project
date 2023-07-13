be46ec3d7f77f0591586e0955365eb7e0cd10981
VladNevgod <vladnewgod@gmail.com>
2023-07-13 17:59:53 +0300

    Edit MainView answer to get 4 records

--- project/wedding/views.py
+++ project/wedding/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+from wedding.models import Wedding
 
 class MainView(TemplateView):
     template_name = 'wedding/main.html'
@@ -8,5 +8,9 @@ class MainView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(MainView, self).get_context_data()
         context['title'] = 'Главная страница'
+        last_id = Wedding.objects.last().id
+        set = Wedding.objects.filter(id__gt=last_id-4)
+        context['weddings'] = set
+
         return context
 
