3fda356d29b0097a1ee015905405cf3a0d92ffe6
ruslanmurav <ruslanmuraveiko@gmail.com>
2023-07-18 20:28:57 +0300

    add 404 error view

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 
-from wedding.views import MainView
+from wedding.views import MainView, pageNotFound
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,4 +13,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = pageNotFound
+
 
