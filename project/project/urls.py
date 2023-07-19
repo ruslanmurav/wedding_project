664a20aba3745d97dce7fce52c1b72821ec8fc34
ruslanmurav <ruslanmuraveiko@gmail.com>
2023-07-19 12:44:24 +0300

    edit comment form

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.views.generic import TemplateView
+from django.views.decorators.cache import cache_page
 
 from wedding.views import MainView, pageNotFound
 
@@ -12,6 +13,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns.append(path("__debug__/", include("debug_toolbar.urls")))
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = pageNotFound
