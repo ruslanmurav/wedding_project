d6a88093e4ac1511059a625ddc753b8e7c1b0cf6
VladNevgod <vladnewgod@gmail.com>
2023-07-26 09:48:06 +0300

    Test beginning

--- project/wedding/views.py
+++ project/wedding/views.py
@@ -83,7 +83,7 @@ class WeddingView(TitleMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super(WeddingView, self).get_context_data()
         context['pk'] = kwargs['pk']
-        context['videos'] = self.get_files(kwargs)
-        context['photos'] = self.get_photos(kwargs)
+        context['videos'] = self.get_files(**kwargs)
+        context['photos'] = self.get_photos(**kwargs)
         return context
 
